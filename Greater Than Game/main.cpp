8fb7bc4cc31e9faac74daa861d74bb5476f1a94e
Ariane Krumel <arianekrumel@gmail.com>
2014-06-15 12:46:40 -0400

    Initial Commit

---
+++ Greater Than Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Greater Than Game
+//
+//  Created by Ariane Krumel on 6/15/14.
+//  Copyright (c) 2014 Ariane Krumel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
